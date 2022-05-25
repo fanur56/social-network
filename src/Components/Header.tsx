07859b078c2ac5d2826ccacad72569055a0d9233
fanur56 <ilasovf@gmail.com>
2022-05-25 18:23:20 +0300

    Added new components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img className="img-logo" alt='pic' src='https://img.freepik.com/free-vector/handdrawn-samurai-logo-mascot_78838-23.jpg?w=826'/>
+        </header>
+    )
+}
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img alt='pic' src='https://mediaproxy.salon.com/width/1200/https://media.salon.com/2022/04/planet-pluto-0405221.jpg'/>
+            </div>
+            <div>
+                Ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
