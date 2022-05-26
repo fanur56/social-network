ea712fedde083f60a0821cc7c2eea2a0e1a5dcae
fanur56 <ilasovf@gmail.com>
2022-05-26 13:05:51 +0300

    Added CSS modules

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
-            <img className="img-logo" alt='pic' src='https://img.freepik.com/free-vector/handdrawn-samurai-logo-mascot_78838-23.jpg?w=826'/>
+        <header className={style.header}>
+            <img className={style.imgLogo} alt='pic' src='https://img.freepik.com/free-vector/handdrawn-samurai-logo-mascot_78838-23.jpg?w=826'/>
         </header>
     )
 }
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './Profile.module.css'
 
 export const Profile = () => {
     return (
-        <div className='content'>
+        <div className={styles.content}>
             <div>
                 <img alt='pic' src='https://mediaproxy.salon.com/width/1200/https://media.salon.com/2022/04/planet-pluto-0405221.jpg'/>
             </div>
@@ -11,12 +12,12 @@ export const Profile = () => {
             </div>
             <div>
                 My posts
-                <div>
+                <div className={styles.posts}>
                     New posts
-                    <div>
+                    <div  className={styles.item}>
                         Post1
                     </div>
-                    <div>
+                    <div className={styles.item}>
                         Post2
                     </div>
                 </div>
